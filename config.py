e132d5c77b150de9037329e789f50d9092840489
Song <satypaldhakad0@gmail.com>
2024-12-31 18:14:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ace_off_peace")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/anime_x_aegis")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/waifexanime")
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "2a230af10e0a40638dc77c1febb47170")
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "7f92897a59464ddbbf00f06cd6bda7fc")
